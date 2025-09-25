254eb8f10b7b3a7615b8ad799d0c080ebc538014
CristianeAlves <crisalves_ef@yahoo.com.br>
2025-09-25 19:01:08 -0300

    Implementação detalhe-produto

--- src/app/pages/detalhe-produto/detalhe-produto.ts
+++ src/app/pages/detalhe-produto/detalhe-produto.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProdutosService } from '../../services/produtos-service';
+import { Produto } from '../../models/produto.interface';
 
 @Component({
   selector: 'app-detalhe-produto',
@@ -6,8 +9,27 @@ import { Component } from '@angular/core';
   templateUrl: './detalhe-produto.html',
   styleUrl: './detalhe-produto.scss'
 })
-export class DetalheProduto {
+export class DetalheProduto implements OnInit {
+  produtoService = inject(ProdutosService);
+  produto: Produto | undefined;
 
-  
+  produtoId: string | null = null;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.produtoId = this.route.snapshot.paramMap.get('id');
+    console.log('ID do produto:', this.produtoId);
+
+    if(this.produtoId) {
+      this.carregaProduto(Number(this.produtoId));
+    }
+  }
+
+  carregaProduto(id: number): void {
+    this.produtoService.getProduto(id).subscribe((produto) => {
+      this.produto = produto;
+    });
+  }
 
 }
